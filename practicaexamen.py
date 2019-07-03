f4d1beecd0f48796dac2b56d91741f08fa8e669b
anabelen2316 <52163970+anabelen2316@users.noreply.github.com>
2019-07-03 13:57:51 +0100

    cosas del examen

--- practicaexamen.py
+++ practicaexamen.py
@@ -1,2 +1,23 @@
 
+class Libro():
+    isnb = ""
+    titulo = ""
+    autor = 0
 
+    def __init__(self, titulo, autor, isbn):
+        pass
+
+class Autor():
+    nombre =""
+    apellido = ""
+    identificador = 0
+    nacimiento = ""
+
+
+
+
+
+
+class Principal():
+    def main(self):
+        pass
